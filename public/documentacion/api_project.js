63805896adf8cde27476ec21e32c1f35605ebd0a
luis plata <www.luisplata@gmail.com>
2017-06-24 00:04:49 -0500

    Actualizando la documentacion

--- public/documentacion/api_project.js
+++ public/documentacion/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.1.0",
   "description": "documentacion para la API de quehaypahacerctg, creado por Luis Plata (www.luisplata@gmail.com)",
   "title": "Documentacion API",
-  "url": "http://quehaypahacer.nabu.com.co",
+  "url": "http://quehaypahacer.nabu.com.co/index.php",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-24T04:51:50.243Z",
+    "time": "2017-06-24T05:04:36.223Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
