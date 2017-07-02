a296708d9826ddd9d14e67b71c3397c34d180f63
Luis Plata <www.luisplata@gmail.com>
2017-07-02 04:50:10 -0500

    Corrigiendo algunos bugs en: Eliminar un usuario, Obtener un usuario. Actualización de Documentación

--- public/documentacion/api_project.js
+++ public/documentacion/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-02T09:48:25.219Z",
+    "time": "2017-07-02T09:50:06.583Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
